6bd390b0986d9f7cf1101598adc15a6d9669af27
xyz600 <xyz600600@gmail.com>
2024-06-22 22:12:37 +0900

    add shader switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { HStack, Heading, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { ShaderSelector } from "./components/ShaderSelector";
+import type { ShaderExampleType } from "./components/ShaderSelector/types";
 import { WebGPUCanvas } from "./components/WebGPUCanvas";
 import { HelloWorld } from "./components/shader/hello-world";
 
 function App() {
+	const [shaderType, setShaderType] =
+		useState<ShaderExampleType>("Hello World");
+
 	return (
-		<>
-			<h1>WebGPU Example</h1>
-			<WebGPUCanvas id="webgpu-canvas">
-				<HelloWorld />
-			</WebGPUCanvas>
-		</>
+		<VStack>
+			<Heading>WebGPU Example</Heading>
+			<HStack>
+				<ShaderSelector
+					currentSelection={shaderType}
+					onSelectionChanged={setShaderType}
+				/>
+				<WebGPUCanvas id="webgpu-canvas">
+					<HelloWorld />
+				</WebGPUCanvas>
+			</HStack>
+		</VStack>
 	);
 }
 
